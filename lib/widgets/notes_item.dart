4e65d4860f4b3d27e930a1e61d33e5fc3ed4ee08
Yasmine Mohsen <yasminemohsen269@gmail.com>
2025-09-20 22:08:02 +0300

    delete note done

--- lib/widgets/notes_item.dart
+++ lib/widgets/notes_item.dart
@@ -41,7 +41,9 @@ class NoteItem extends StatelessWidget {
               ),
 
               trailing: IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  note.delete();
+                },
                 icon: Icon(Icons.delete, color: Colors.black, size: 26),
               ),
             ),
